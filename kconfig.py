850df03f1dd0172609c600247cf3f39f1a276e42
Krishna Dwivedi <150330334+SudoHopeX@users.noreply.github.com>
2025-09-21 16:17:12 +0530

    Create kconfig.py

---
+++ kconfig.py
@@ -0,0 +1,9 @@
+"""API keys for Gemini and ChatGPT"""
+
+
+"""paste your GEMINI API KEY in place of None"""
+GEMINI_API_KEY = None
+
+
+"""paste your OPENAI (ChatGPT) API KEY in place of None"""
+OPENAI_API_KEY = None
