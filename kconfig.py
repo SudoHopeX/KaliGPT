4c12c74652988a2a695dc686f2b1e18d4cba4a52
Krishna Dwivedi <150330334+SudoHopeX@users.noreply.github.com>
2025-12-16 21:05:33 +0530

    Delete kconfig.py

--- kconfig.py
+++
@@ -1,9 +0,0 @@
-"""API keys for Gemini and ChatGPT"""
-
-
-"""paste your GEMINI API KEY in place of None"""
-GEMINI_API_KEY = None
-
-
-"""paste your OPENAI (ChatGPT) API KEY in place of None"""
-OPENAI_API_KEY = None
